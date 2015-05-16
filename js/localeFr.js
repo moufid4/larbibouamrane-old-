02d415b133dbb06e5697d5aa4d5df06286ac7bc9
Moufid Larbi <moufid4@gmail.com>
2015-05-16 18:12:43 +0100

    disabling weekend and changing first day of the week

--- js/localeFr.js
+++ js/localeFr.js
@@ -51,7 +51,7 @@
             return number + (number === 1 ? 'er' : '');
         },
         week : {
-            dow : 1, // Monday is the first day of the week.
+            dow : 0, // Monday is the first day of the week.
             doy : 4  // The week that contains Jan 4th is the first week of the year.
         }
     });
