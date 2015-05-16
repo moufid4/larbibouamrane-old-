2b6cfb65d6cea9706b878debceec2d8448d6ca8c
Moufid Larbi <moufid4@gmail.com>
2015-05-16 01:22:35 +0100

    adding appointment form

--- js/appointment.js
+++ js/appointment.js
@@ -1,13 +1,19 @@
- jQuery('#datetimepicker').datetimepicker({
-                onSelectDate: function (ct) {console.log(ct.dateFormat('d/m/Y'))},
-                onSelectTime: function (ct) {console.log(ct.dateFormat('H:i'))},
-                lang:'fr',
-                onGenerate:function( ct ){
-                jQuery(this).find('.xdsoft_date.xdsoft_weekend')
-                  .addClass('xdsoft_disabled');
-                  },
-              weekends:[],
-              timepicker:true
-                
+$('#datetimepicker').datetimepicker({
+    onSelectDate: function (ct) {
+    	dateSelect = ct.dateFormat('d/m/Y');
+},
+    onSelectTime: function (ct) {
+    	timeSelect = ct.dateFormat('H:i');
+},  
+    onGenerate:function( ct ){
+	    $(this).find('.xdsoft_date.xdsoft_weekend')
+	      .addClass('xdsoft_disabled');
+	},
 
-            });
+	weekends:[],
+	timepicker:true,
+	lang:'fr',
+});
+
+var dateSelect;
+var timeSelect;
