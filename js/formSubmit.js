4ab7dc2a4465c24275681cbce9e93088967b0c34
Moufid Larbi <moufid4@gmail.com>
2015-05-17 02:16:21 +0100

    changing color date appointment

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 		        $('#appointmentSection').toggleClass('show hidden');
 
 
-		        $("#time").html("<h3>Excellent! Votre rendez-vous avec Me Larbi Bouamrane est le </h3>" + date + "<h3>Vous recevrez un email de confirmation dans quelques instants.</h3>");
+		        $("#time").html(date);
 
                  }
     });
