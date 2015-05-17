09f7ca4ab83090677a72a02b84e2a48a5c30cc10
Moufid Larbi <moufid4@gmail.com>
2015-05-17 17:09:54 +0100

    setting up send email confirmation

--- server.js
+++ server.js
@@ -1,5 +1,42 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'moufid4@gmail.com',
+        pass: 'test'
+    }
+});
+
+	app.post('/email', function(req, res) {
+		var mailOptions = {
+		    from: 'Moufid Larbi ✔ <moufid4@gmail.com>', // sender address
+		    to: 'moufid4@gmail.com', // list of receivers
+		    subject: 'Success! ✔', // Subject line
+		    text: 'Hello world ✔', // plaintext body
+		    html: '<b>Hello world ✔</b>' // html body
+		};
+
+		transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+
+		
+		
+		
+		});
+
+	});
+
+
+
+
 
 app.use(express.static(__dirname + '/'));
 
