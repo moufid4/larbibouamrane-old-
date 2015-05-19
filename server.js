d4f9bc1d1d2c676d2d575c2f2cc59c738e1a9bc9
Moufid Larbi <moufid4@gmail.com>
2015-05-19 17:32:19 +0100

    replacing top navigation

--- server.js
+++ server.js
@@ -97,10 +97,8 @@ app.post('/fr/email', function(req, res) {
 app.use(express.static(__dirname + '/'));
 
 app.get('fr/', function (req, res) {
-  res.send('fr/index.html');
 });
-app.get('/en', function (req, res) {
-  res.send('en/index.html');
+app.get('en/', function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000);
